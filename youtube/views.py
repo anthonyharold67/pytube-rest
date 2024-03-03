d275d3eab080d4a12ebabf42c3c55baa0a63f85c
anthonyharold67 <anthonyharold67@gmail.com>
2024-03-03 19:35:30 +0300

    fixed

--- youtube/views.py
+++ youtube/views.py
@@ -1,35 +1,77 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from pytube import *
+# from django.http import HttpResponse
+# from django.shortcuts import render
+# from pytube import *
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# import time
+# from hurry.filesize import size #dosya boyutunu görüntü değiştirmek için
+# # Create your views here.
+
+# @api_view(['GET'])
+# def youtube(request):
+#     if request.method == 'GET':
+#         url = request.GET.get('url')
+#         if url:
+#             yt=YouTube(url)
+#             video={
+#                 "info":{
+#                     "title":yt.title,
+#                     "thumbnail":yt.thumbnail_url,
+#                     "description":yt.description,
+#                     "author":yt.author,
+#                     "published":yt.publish_date,
+#                     "views":yt.views,
+#                     "length":time.strftime('%H:%M:%S',time.gmtime(yt.length)),
+#                 },
+#                 "sources":[]
+#             }
+#             videos = yt.streams.filter(progressive=True)
+#             for v in videos:
+#                 video["sources"].append({
+#                     "url":v.url,
+#                     "size":size(v.filesize),
+#                     "quality":v.resolution,
+#                 })
+#         return Response(video)
+
+from django.http import JsonResponse
+from pytube import YouTube
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from hurry.filesize import size
 import time
-from hurry.filesize import size #dosya boyutunu görüntü değiştirmek için
-# Create your views here.
 
 @api_view(['GET'])
 def youtube(request):
     if request.method == 'GET':
         url = request.GET.get('url')
         if url:
-            yt=YouTube(url)
-            video={
-                "info":{
-                    "title":yt.title,
-                    "thumbnail":yt.thumbnail_url,
-                    "description":yt.description,
-                    "author":yt.author,
-                    "published":yt.publish_date,
-                    "views":yt.views,
-                    "length":time.strftime('%H:%M:%S',time.gmtime(yt.length)),
-                },
-                "sources":[]
-            }
-            videos = yt.streams.filter(progressive=True)
-            for v in videos:
-                video["sources"].append({
-                    "url":v.url,
-                    "size":size(v.filesize),
-                    "quality":v.resolution,
-                })
-        return Response(video)
+            try:
+                yt = YouTube(url)
+                video_info = {
+                    "info": {
+                        "title": yt.title,
+                        "thumbnail": yt.thumbnail_url,
+                        "description": yt.description,
+                        "author": yt.author,
+                        "published": yt.publish_date,
+                        "views": yt.views,
+                        "length": time.strftime('%H:%M:%S', time.gmtime(yt.length)),
+                    },
+                    "sources": []
+                }
+                
+                videos = yt.streams.filter(progressive=True)
+                for v in videos:
+                    video_info["sources"].append({
+                        "url": v.url,
+                        "size": size(v.filesize),
+                        "quality": v.resolution,
+                    })
+                    
+                return JsonResponse(video_info)
+            
+            except Exception as e:
+                return JsonResponse({"error": str(e)}, status=500)
+        
+        else:
+            return JsonResponse({"error": "URL parametresi eksik."}, status=400)
